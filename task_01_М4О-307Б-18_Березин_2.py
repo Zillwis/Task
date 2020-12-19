89ff5422e3aba5260b5225518fed9fb95db3e94c
Zillwis <71565574+Zillwis@users.noreply.github.com>
2020-12-19 22:33:46 +0300

    Add files via upload

---
+++ task_01_М4О-307Б-18_Березин_2.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import os
+
+#Папка для txt файла  результатами
+try:
+ os.mkdir('results')
+except OSError:
+ pass
+
+complete_file = os.path.join('results', 'task_02_307Б_Березин.txt')
+print("Текущая деректория:", os.getcwd())
+
+#Заданная функция
+def f(x):
+    return -20*np.exp(-0.2*np.sqrt(0.5*x**2))-np.exp(0.5*(np.cos(2*math.pi*x)+1))+np.e+20
+
+for x in list(range(-5, 6, 1)):
+    y=-20*np.exp(-0.2*np.sqrt(0.5*x**2))-np.exp(0.5*(np.cos(2*math.pi*x)+1))+np.e+20
+    print(x,y)
+
+x= np.linspace(-5,5,300)
+y=f(x)
+
+plt.plot(x, y)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.grid()
+plt.show()
+
